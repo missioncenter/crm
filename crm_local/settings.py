38afd62392b65d5d28fdd7be082c1474c4e7b22f
Rustam <s.rustam.a@gmail.com>
2026-04-22 11:59:46 +0500

    fix: Update logout redirect URL to point to the login page

--- crm_local/settings.py
+++ crm_local/settings.py
@@ -70,7 +70,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/accounts/login/"
 
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
--- crm_local/urls.py
+++ crm_local/urls.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth import logout
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("/accounts/login/")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/logout/", logout_view, name="logout"),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("projects.urls")),
 ]
